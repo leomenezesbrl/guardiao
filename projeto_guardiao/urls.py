1197e16d0d8f9a40903f393f8f59786438c400d0
Leonardo Pimentel <leonardo-eb@hotmail.com>
2025-01-06 08:18:56 -0400

    quase 3

--- projeto_guardiao/urls.py
+++ projeto_guardiao/urls.py
@@ -7,3 +7,30 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('guardiao.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+from django.http import JsonResponse
+
+def debug_view(request):
+    import os
+    import django
+    from django.db import connection
+
+    data = {
+        'DJANGO_SETTINGS_MODULE': os.environ.get('DJANGO_SETTINGS_MODULE'),
+        'DATABASE_URL': os.environ.get('DATABASE_URL'),
+        'DEBUG': os.environ.get('DEBUG'),
+        'SECRET_KEY': os.environ.get('SECRET_KEY'),
+        'DB_CONNECTION_TEST': False,
+    }
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT 1;")
+        data['DB_CONNECTION_TEST'] = True
+    except Exception as e:
+        data['DB_CONNECTION_ERROR'] = str(e)
+
+    return JsonResponse(data)
+
+urlpatterns += [
+    path('debug/', debug_view),
+]
