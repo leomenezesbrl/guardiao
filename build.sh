de66a5c102241c90108cd5c7a2d24982ad011b70
Leonardo Pimentel <leonardo-eb@hotmail.com>
2025-01-06 11:50:31 -0400

    quase 5

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Instala as dependências
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
+
+# Coleta os arquivos estáticos
+python manage.py collectstatic --noinput
+
+# Aplica as migrações do banco de dados
 python manage.py migrate
